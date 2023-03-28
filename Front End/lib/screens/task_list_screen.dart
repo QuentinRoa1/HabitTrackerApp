b233f301dee9ba97edaccc87f4bb78f33ada28a7
QuentinRoa1 <98721340+QuentinRoa1@users.noreply.github.com>
2023-03-28 01:27:45 -0400

    Made actually work
    changed some things, ternary operators do not like being in a column, especially when there is no set height for the list view. Added a container around it to mitigate.

--- Front End/lib/screens/task_list_screen.dart
+++ Front End/lib/screens/task_list_screen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: depend_on_referenced_packages
 // TODO IMPLEMENT THIS CLASS
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/ui/showform.dart';
 import 'package:habit_tracker/ui/cardwidget.dart';
@@ -23,13 +25,13 @@ class _TaskListState extends State<TaskList> {
   final String dbname;
   late var db;
   late var show;
+  int _selectedIndex = 0;
 
   _TaskListState({required this.dbname}) {
     _items = [];
     db = Database(db: dbname);
     show = ShowForm(db: db, refreshItems: _refreshItems);
   }
-
   @override
   void initState() {
     super.initState();
@@ -53,11 +55,17 @@ class _TaskListState extends State<TaskList> {
     _refreshItems();
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ASE456'),
+        title: const Text('Habit Tracker'),
         actions: [
           IconButton(
             icon: const Icon(Icons.refresh),
@@ -79,27 +87,56 @@ class _TaskListState extends State<TaskList> {
       body: Column(
         children: [
           SearchInput(),
-          _items.isEmpty
-              ? const Center(
-                  child: Text(
-                    'No Data',
-                    style: TextStyle(fontSize: 30),
+          Container(
+            height: MediaQuery.of(context).size.height -
+                MediaQuery.of(context).size.height / 2.5,
+            child: _items.isEmpty
+                ? const Center(
+                    child: Text(
+                      'No Data',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  )
+                : ListView.builder(
+                    // the list of items
+                    itemCount: _items.length,
+                    itemBuilder: (_, index) {
+                      final currentItem = _items[index];
+                      return CardWidget(currentItem, show, deleteItem);
+                    },
                   ),
-                )
-              : ListView.builder(
-                  // the list of items
-                  itemCount: _items.length,
-                  itemBuilder: (_, index) {
-                    final currentItem = _items[index];
-                    return CardWidget(currentItem, show, deleteItem);
-                  },
-                ),
+          ),
         ],
       ),
+
       // Add new item button
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.extended(
         onPressed: () async => await show.showForm(context, null),
-        child: const Icon(Icons.add),
+        label: const Text("New Task"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.format_list_numbered),
+            label: 'Leaderboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard),
+            label: 'Statistics',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 142, 16, 221),
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
