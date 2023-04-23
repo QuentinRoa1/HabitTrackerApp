96eef31b52038f768c0a5f0eecb0c88c982bac7f
Hunter Giordullo <98577171+Giordullo@users.noreply.github.com>
2023-04-22 20:13:32 -0400

    Add files via upload

---
+++ Back End/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Habit Tracker App</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	<style>
+		body {
+			background-color: #F4F4F4;
+		}
+
+		.jumbotron {
+			background-color: #fff;
+			padding: 2rem 1rem;
+			margin-bottom: 2rem;
+			border-radius: 0.3rem;
+		}
+	</style>
+</head>
+<body>
+	<div class="container mt-5">
+		<div class="jumbotron">
+			<h1 class="display-4">Habit Tracker App</h1>
+			<p class="lead">“Develop better habits for a brighter future.”</p>
+			<hr class="my-4">
+			<p>Download now and develop better habits!</p>
+			<a class="btn btn-primary btn-lg" href="#" role="button">Download</a>
+			<a class="btn btn-primary btn-lg" href="/ASE485/api/document.php?session=r7VNNrUrsiHhEMsNa8OVqHB77uFKAHq8AnAhxXO55qDwW0JquvuL7BSwciiJQ48p" role="button">Documentation</a>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
